a3ad189886d170c88468e3e076eda2438e39fd99
mazm <mazm@ziroom.com>
2019-07-06 18:20:21 +0800

    创建一个工程

---
+++ Autotest/src/main/java/com/learning/testing/BasicAnno.java
@@ -0,0 +1,13 @@
+package com.learning.testing;
+
+
+import org.testng.annotations.Test;
+
+
+class BasicAnno {
+    @Test
+    public void testCase1(){
+        System.out.println("这是一个最简单的测试用例");
+    }
+}
+
