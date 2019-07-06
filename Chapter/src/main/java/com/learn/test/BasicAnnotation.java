b5d126f3b4382d2f8eddb59ef5983adb56a1c3ba
mazm <mazm@ziroom.com>
2019-07-06 18:54:02 +0800

    提交第一个测试

---
+++ Chapter/src/main/java/com/learn/test/BasicAnnotation.java
@@ -0,0 +1,10 @@
+package com.learn.test;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+    @Test
+    public void testCase(){
+        System.out.println("第一个测试");
+    }
+}
