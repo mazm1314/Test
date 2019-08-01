feb245b10beded2a22f883339e49ab1b2a9ff6a2
mazm <mazm@ziroom.com>
2019-08-01 17:08:34 +0800

    提交十一章

--- chapter11/src/main/java/com/course/Application.java
+++ chapter11/src/main/java/com/course/Application.java
@@ -12,11 +12,13 @@ import javax.annotation.PreDestroy;
 @EnableScheduling
 @SpringBootApplication
 public class Application {
+
     private static ConfigurableApplicationContext context;
 
     public  static  void main(String[] args){
         Application.context = SpringApplication.run(Application.class,args);
     }
+
     @PreDestroy
     public void close(){
         Application.context.close();
--- chapter11/src/main/java/com/course/controller/Demo.java
+++ chapter11/src/main/java/com/course/controller/Demo.java
@@ -1,10 +1,12 @@
 package com.course.controller;
 
 
+import com.course.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Log4j
@@ -14,12 +16,46 @@ import org.springframework.web.bind.annotation.*;
 public class Demo {
 //    首先获得一个执行sql的对象
 
+//    这个注解的作用是启动类加载，就是demo启动起来，这个类就被加载
+    @Autowired
     private SqlSessionTemplate template;
 
+    /**
+     * http://localhost:8888/v1/getUserCount
+     * @return   返回user表的存的数的个数
+     */
     @RequestMapping(value = "/getUserCount",method = RequestMethod.GET)
     @ApiOperation(value = "可以获取到用户数",httpMethod = "GET")
     public int getUserCount(){
-        return template.selectOne("getUserCount");
+         int result=template.selectOne("getUserCount");
+        return result;
     }
+
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
+    @ApiOperation(value = "增加用户",httpMethod = "POST")
+    public int  addUser(@RequestBody User user){
+        int result=template.insert("addUser",user);
+        return  result;
+    }
+
+    @RequestMapping(value ="/updateUser",method =RequestMethod.POST)
+    @ApiOperation(value = "跟新用户信息",httpMethod = "post")
+    public int updateUser(@RequestBody User user){
+        int result=template.update("updateUser",user);
+        return result;
+    }
+
+    /**
+     *
+     * @param id
+     * @return  返回删除了几条数据的个数
+     */
+    @RequestMapping(value ="/deleteUser",method =RequestMethod.GET)
+    @ApiOperation(value = "删除用户信息",httpMethod = "GET")
+    public int deleteUser(@RequestParam int id){
+        int result=template.delete("deleteUser",id);
+        return result;
+    }
+
 
 }
