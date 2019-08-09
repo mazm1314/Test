218d5aaa72ab5f4948413d956e29b7f9042402e7
mazm <mazm@ziroom.com>
2019-08-09 16:23:36 +0800

    提交增删改查

--- Chapter13/src/main/java/com/course/Application.java
+++ Chapter13/src/main/java/com/course/Application.java
@@ -3,17 +3,23 @@ package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+import javax.annotation.PreDestroy;
+
 @EnableScheduling
+@SpringBootApplication
 public class Application {
 
-    public  static void main(String args[]){
-        SpringApplication.run(Application.class,args);
+    private static ConfigurableApplicationContext context;
+
+    public static void main(String[] args) {
+        Application.context = SpringApplication.run(Application.class, args);
     }
 
-
-
+    @PreDestroy
+    public void close() {
+        Application.context.close();
+    }
 }
-
